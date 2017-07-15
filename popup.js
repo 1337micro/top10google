7acd06e93a06667c5b406b9332ff80ab62790acf
William Prioriello <williamap@gmail.com>
2017-07-15 01:36:49 -0400

    done

--- popup.js
+++ popup.js
@@ -1,20 +1,27 @@
-
+document.getElementById("submit_button").onclick = search
 function search(){
+  var sSearchURI = "http://www.google.com/search?q=";
+  var sSearchQuery = document.getElementById("search_box").value;
+  var aSearchTerms = sSearchQuery.split();
+
+  aSearchTerms.forEach( (sTerm) =>{
+    sSearchURI += sTerm + "+"; //append search terms to the sSearchURI
+  });
+  sSearchURI = sSearchURI.slice(0, sSearchURI.length -1); // removes the last +
+
+  chrome.tabs.create({url:sSearchURI}); // main search results
+  chrome.tabs.executeScript(null, {
+    file: 'getTabHtmlScript.js'
+  });
+  /*
  //chrome.tabs.create(object createProperties, function callback)
  console.log("popupjs loaded");
 
-var sSearchURI = "http://www.google.com/search?q=";
-var sSearchQuery = document.getElementById("search_box").value;
-var aSearchTerms = sSearchQuery.split();
-
-aSearchTerms.forEach( (sTerm) =>{
-  sSearchURI += sTerm + "+"; //append search terms to the sSearchURI
-});
-sSearchURI = sSearchURI.slice(0, sSearchURI.length -1); // removes the last +
 
 console.log(sSearchURI);
 //getHtmlOfSearch(sSearchURI);
 chrome.tabs.create({url:sSearchURI}); // main search results
+*/
 
 
 };
